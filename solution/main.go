f20b3f51300b5f2d54eeeb57b0479315755ed181
Dubomb <dubomb1011@gmail.com>
2026-03-21 17:08:35 -0400

    Implemented file reading with an example file)

--- solution/main.go
+++ solution/main.go
@@ -6,18 +6,16 @@ import (
 	"os"
 )
 
-func repl() {
+func repl(env Env) {
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("Beginning REPL. Type \"quit\" to exit.")
-
-	env := get_starting_env()
 
 	for {
 		fmt.Print("> ")
 		input, err := reader.ReadString('\n')
 
-		if input == "quit" {
+		if input == "quit\n" {
 			break
 		}
 
@@ -46,6 +44,57 @@ func repl() {
 	}
 }
 
+func file(filename string, env Env) error {
+	program, err := os.Open(filename)
+
+	if err != nil {
+		return fmt.Errorf("Failed to open file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(program)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			continue
+		}
+
+		parsed, err := parse(line)
+
+		if err != nil {
+			return fmt.Errorf("Parse error: failed to parse input.\n%s\n", err)
+		}
+
+		evaled, err := eval(parsed, env)
+
+		if err != nil {
+			return fmt.Errorf("Semantic error: failed to evaluate input.\n%s\n", err)
+		}
+
+		if evaled != nil {
+			fmt.Println(evaled)
+		}
+	}
+
+	program.Close()
+
+	return nil
+}
+
 func main() {
-	repl()
+	env := get_starting_env()
+
+	if len(os.Args) > 1 {
+		filename := os.Args[1]
+		err := file(filename, env)
+
+		if err != nil {
+			fmt.Printf("Error encountered while running file.\n%s\n", err)
+			return
+		}
+	} else {
+		repl(env)
+	}
+
 }
